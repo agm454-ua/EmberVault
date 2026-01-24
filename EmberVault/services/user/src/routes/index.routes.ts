2c2db3b6694f1973e47dd63da7d4f7c0ad2af15e
Alex <agm454@alu.ua.es>
2026-01-24 18:12:45 +0100

    Documented routes

--- EmberVault/services/user/src/routes/index.routes.ts
+++ EmberVault/services/user/src/routes/index.routes.ts
@@ -3,7 +3,15 @@ import { Router } from "express";
 
 const router = Router()
 
-
+/*
+    GET     /users/count
+    GET     /users/all
+    GET     /users/added-last-week
+    POST    /users
+    GET     /users/:user
+    PUT     /users/:user
+    DELETE  /users/:user
+*/
 
 
 
