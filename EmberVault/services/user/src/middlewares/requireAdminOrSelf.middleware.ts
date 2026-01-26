bd67e1dafb8932a3ff601be791a19ec2d751d39f
Alex <agm454@alu.ua.es>
2026-01-27 00:39:02 +0100

    Middlewares

---
+++ EmberVault/services/user/src/middlewares/requireAdminOrSelf.middleware.ts
@@ -0,0 +1,36 @@
+import { sendErrorResponse, sendUnauthorizedResponse } from "@agm454-ua/auth-utils"
+import { getAdminRole } from "@services/system_roles.service.js"
+import type { NextFunction, Response, Request } from "express"
+
+const requireAdmin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    const payload = req.user
+    const targetUserId =  req.params.userId as string
+    
+    if (!payload) {
+        return sendErrorResponse(res)
+    }
+
+    // check if the target user is the user sending the request
+    if (targetUserId === payload.userId) {
+        next()
+    }
+
+    // check if admin
+    const adminRole = await getAdminRole()
+
+    if (!adminRole) {
+        return sendErrorResponse(res)
+    }
+
+    if (payload.systemRole !== adminRole) {
+        return sendUnauthorizedResponse(res)
+    }
+
+    next()
+}
+
+export default requireAdmin
