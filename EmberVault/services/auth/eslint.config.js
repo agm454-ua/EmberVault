e1062abab736eaa456c0ea548e11a382fe64ca90
Alex <123944999+agm454-ua@users.noreply.github.com>
2026-01-28 00:17:43 +0100

    Fixed: (#18)
    - declaration file extension
     - removed unnecessary dependencies
     - eslint config

--- EmberVault/services/auth/eslint.config.js
+++ EmberVault/services/auth/eslint.config.js
@@ -1,15 +1,27 @@
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default [
     js.configs.recommended,
-
     ...tseslint.configs.recommended,
+
+    {
+        languageOptions: {
+            parserOptions: {
+                tsconfigRootDir: __dirname,
+                project: './tsconfig.json',
+            },
+        },
+    },
 
     prettier,
 
     {
-        ignores: ['dist/', 'node_modules/'],
+        ignores: ['dist/', 'node_modules/', 'eslint.config.js', 'tests/'],
     },
 ]
