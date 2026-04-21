49f276d336a6a8bf2d1f2ee6e04c2f9b9eecac4c
Alex <agm454@alu.ua.es>
2026-04-22 00:03:00 +0200

    feat: added default profile picture

--- EmberVault/web/src/features/user/components/UpdateProfilePicture.tsx
+++ EmberVault/web/src/features/user/components/UpdateProfilePicture.tsx
@@ -22,9 +22,10 @@ export default function UpdateProfilePicture({
 				alt={alt}
 				className={`aspect-square rounded-full border-stroke object-cover h-32 w-32 ${disabled ? '' : 'group-hover:blur-xs'} ${className}`}
 			/>
+			<div className={`absolute inset-0 rounded-full bg-black/20 opacity-0 ${disabled ? '' : 'group-hover:opacity-100'} transition-opacity`} />
 			<div className={`absolute inset-0 flex flex-col items-center justify-center opacity-0 ${disabled ? '' : 'group-hover:opacity-100'} transition-opacity`}>
-				<UploadImageIcon className="text-ink-muted z-10" />
-				<p className="text-xs text-ink-muted text-center">{t('profilePage.uploadProfilePicture')}</p>
+				<UploadImageIcon className="text-ink-inverse z-10" />
+				<p className="text-xs text-ink-inverse text-center">{t('profilePage.uploadProfilePicture')}</p>
 			</div>
 		</div>
 	)
