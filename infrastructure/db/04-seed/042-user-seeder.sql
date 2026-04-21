ef9fe6f1b0cfe0483bc9d1bb8321f67f9577065f
Alex <agm454@alu.ua.es>
2026-04-21 17:45:23 +0200

    fix: database user seeder matches new password requirements and root folder trigger now assigns the value to the user

--- infrastructure/db/04-seed/042-user-seeder.sql
+++ infrastructure/db/04-seed/042-user-seeder.sql
@@ -1,13 +1,17 @@
 INSERT INTO users (username, email, password, birth_date, system_role)
 VALUES 
-    ('admin', 'agm454@alu.ua.es', '$2b$12$7B.RK6rqse4lmlmNH0tvCuOpg7bcmS0N3Md96qPxwxxpFSdjri5iK', '2004-04-08',
+    ('admin', 'agm454@alu.ua.es', '$2a$10$woaA9tvuWUF6rN.MesIgduCrAPSPwmKXsvS5rT1rfqAPOR.kMTLVu', '2004-04-08',
         (SELECT id FROM system_roles WHERE name='admin')
     ),
 
-    ('user1', 'user1@example.com', '$2b$12$tXDay9A/wQkl./QK4pk38epLIFcjkPiuDouCTZ8ZWIpP1OVuiKM1a', '1900-01-01', -- password = user123
+    ('user1', 'user1@example.com', '$2a$10$9PxQ2qcQ5Y.Xe8ghELPwfu5n56wqV7na6PrAmydo/Np4OZZWv3rIa', '1900-01-01', -- password = User123!
         (SELECT id FROM system_roles WHERE name='user')
     ),
 
-    ('user2', 'user2@example.com', '$2b$12$0Z6dOtlY17.JmH9zvw5KEuI1ociT8qJkLtOpoDvsQ0OCiXOYzcU8y', '1900-01-01', -- password = user234
+    ('user2', 'user2@example.com', '$2a$10$CjzuB3pcx5U0JCJ3RXh3v./SDTRhwL6SenjBGoRPVFkmiLJX52MYq', '1900-01-01', -- password = User234!
+        (SELECT id FROM system_roles WHERE name='user')
+    ),
+
+    ('test', 'test@test.com', '$2a$10$ODNVeod70me/wRlbmdFRY.2myabgS.MtT8ihuQ9iLoXqzml3Crg8m', '2004-04-08',
         (SELECT id FROM system_roles WHERE name='user')
     );
